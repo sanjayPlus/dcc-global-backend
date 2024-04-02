ad6c49092f4e470517234016e6183b23a1141048
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-04-02 12:22:21 +0530

    new commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const adminSchema = new mongoose.Schema({
-    username: String,
+    email: String,
     password: String,
     loginDates: [
         {
@@ -32,10 +32,26 @@ const Admin = mongoose.model('Admin', adminSchema);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+app.get('/api/admin/protected', async (req, res) => {
+    try {
+        const token = req.headers['x-access-token'];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const admin = await Admin.findById(decoded.id);
+        if (!admin) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        res.status(200).json({ message: 'Protected data' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
 app.post('/api/admin/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const admin = await Admin.findOne({ username });
+        const { email, password } = req.body;
+        const admin = await Admin.findOne({ email });
         if (!admin) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
@@ -55,12 +71,30 @@ app.post('/api/admin/login', async (req, res) => {
     }
 })
 
-app.get('/api/admin/get-backend-url', (req, res) => {
+app.get('/api/admin/get-backend-url',async (req, res) => {
     try {
+
             const {district}= req.params;
             if(!district){
                 return res.status(401).json({ message: 'Invalid credentials' });
             }
+     const token = req.headers['x-access-token'];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const admin = await Admin.findById(decoded.id);
+        if (!admin) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        if(district = "Thrissur"){
+          const tokenRes = await  axios.get('https://dcctcr-backend.plusitpark.com/api/admin/login-from-dcc-admin',{
+                headers: {
+                    'x-access-token': jwt.sign({ id: admin._id }, process.env.VOLUNTEER_SERVER_SECRET, { expiresIn: '365d' })
+                }
+            })
+         return  res.status(200).json({ token: tokenRes.data.token, district: "Thrissur" });
+        }
 
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });

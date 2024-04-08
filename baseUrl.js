16cc8b13bc128443bfa9ad84a7768a00a850b568
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-04-08 13:05:09 +0530

    https in url added

--- baseUrl.js
+++ baseUrl.js
@@ -5,22 +5,22 @@ const baseUrl = [{
     url: 'https://dcctcr-backend.plusitpark.com',
     district: 'Thrissur',
 },{
-    url:'http://dcctvm-backend.plusitpark.com',
+    url:'https://dcctvm-backend.plusitpark.com',
     district: 'Thiruvananthapuram',
 },{
     url:'https://dccpkd-backend.plusitpark.com',
     district:'Palakkad'
 },{
-    url:'http://dccmlp-backend.plusitpark.com',
+    url:'https://dccmlp-backend.plusitpark.com',
     district:'Malappuram'
 },{
     url:'https://dcckoz-backend.dmckpcc.in',
     district:'Kozhikode'
 },{
-    url:'dccway-backend.plusitpark.com',
+    url:'https://dccway-backend.plusitpark.com',
     district:'Wayanad'
 },{
-    url:'http://dccknr-backend.plusitpark.com',
+    url:'https://dccknr-backend.plusitpark.com',
     district:'Kannur'
 },{
     url:'https://dccksg-backend.plusitpark.com',
